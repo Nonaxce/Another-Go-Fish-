4492d6d37f1abb5f9384d5aa3ebd0d1dbba295e2
mnota <124483407+Nonaxce@users.noreply.github.com>
2026-01-04 13:56:25 +0800

    Replace ws with socket.io (too verbose (and the other too bloated))

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 import express from "express";
 import { createServer } from "http";
-import { WebSocketServer } from "ws"
+import { Server } from "socket.io";
 
+// import routes
 import { indexRouter } from "./routes/indexRouter.js";
 
+// setup server
 const app = express()
 const server = createServer(app);
+const io = new Server(server);
 
+// static files
 app.use(express.static('public'))
 
+// index
 app.get("/", indexRouter)
 
+io.on("connection", (socket) => {
+    console.log("A user connected")
+})
+
+
+
+// start server
 const PORT = process.env.PORT || 3000;
 const LOCAL_IP = process.env.LOCAL_IP || ""
-app.listen(PORT, LOCAL_IP, (error) => {
+server.listen(PORT, LOCAL_IP, (error) => {
     if (error) {
         throw error
     }
